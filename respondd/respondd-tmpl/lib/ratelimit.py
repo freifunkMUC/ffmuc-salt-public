f5b797aeee4064c1f36f9db6a9dc745ff501009c
awlx <aw@awlnx.space>
2021-04-30 22:39:27 +0200

    Run Black again and blacklist an unfixable file
    This excludes `respondd/respondd-tmpl/lib/respondd_client.py` from the
    linter which maybe ... can be fixed later
    
    Add yamllint

--- respondd/respondd-tmpl/lib/ratelimit.py
+++ respondd/respondd-tmpl/lib/ratelimit.py
@@ -2,30 +2,31 @@
 
 import time
 
-class rateLimit: # rate limit like iptables limit (per minutes)
-  tLast = None
 
-  def __init__(self, _rate_limit, _rate_burst):
-    self.rate_limit = _rate_limit
-    self.rate_burst = _rate_burst
-    self.bucket = _rate_burst
+class rateLimit:  # rate limit like iptables limit (per minutes)
+    tLast = None
 
-  def limit(self):
-    tNow = time.time()
+    def __init__(self, _rate_limit, _rate_burst):
+        self.rate_limit = _rate_limit
+        self.rate_burst = _rate_burst
+        self.bucket = _rate_burst
 
-    if self.tLast is None:
-      self.tLast = tNow
-      return True
+    def limit(self):
+        tNow = time.time()
 
-    tDiff = tNow - self.tLast
-    self.tLast = tNow
+        if self.tLast is None:
+            self.tLast = tNow
+            return True
 
-    self.bucket += (tDiff / (60 / self.rate_limit))
-    if self.bucket > self.rate_burst:
-      self.bucket = self.rate_burst
+        tDiff = tNow - self.tLast
+        self.tLast = tNow
 
-    if self.bucket >= 1:
-      self.bucket-= 1
-      return True
-    else:
-      return False
+        self.bucket += tDiff / (60 / self.rate_limit)
+        if self.bucket > self.rate_burst:
+            self.bucket = self.rate_burst
+
+        if self.bucket >= 1:
+            self.bucket -= 1
+            return True
+        else:
+            return False
