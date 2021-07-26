83ff668ee47a07702d03155dbed2abf27b6316ff
salt-master <hilfe@ffmuc.net>
2021-07-26 19:47:33 +0200

    systemd-networkd: use logger when overwriting througput_override

--- systemd-networkd/files/batadv-throughput.sh
+++ systemd-networkd/files/batadv-throughput.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for i in `ip -br a | grep vx- | awk '{print $1}'`; do 
-    echo "Set throughput_override on $i"
+    logger -t batadv-override "Set throughput_override on $i"
     batctl hardif $i throughput_override 1000000
 done
